0f45e8be9a813580b31a8f07e6928e5de1f50076
Eloyse Becker <elo.becker.13@gmail.com>
2025-03-05 11:07:09 -0300

    alterando variaveis

---
+++ aulaGit.dart
@@ -0,0 +1,18 @@
+main(){
+/**
+*tipos numéricos 
+*int - numero inteiros sem casa decimais
+*double - numeros com ponto flutuante e casa decimais
+ */
+ int idade = 18;
+ double altura = 1.80;
+
+ String pessoa = "Maria";
+ double salario = 49.560;
+
+ int ano = 2025;
+ print(idade);
+}
+
+
+
